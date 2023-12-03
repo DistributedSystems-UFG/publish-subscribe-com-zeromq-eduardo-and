39b0425eb09c57a12fe34c61d7b8a9061fc79a92
Eduardo Andrade <eduardo.ufg@hotmail.com>
2023-12-03 14:42:43 -0300

    ✨ Add new topics and subscribers

--- publisher.py
+++ publisher.py
@@ -1,11 +1,21 @@
+from datetime import datetime
 import zmq, time
-from constPS import * #-
+from constPS import *  # -
 
 context = zmq.Context()
-s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://0.0.0.0:"+ PORT      # how and where to communicate
-s.bind(p)                          # bind socket to the address
+s = context.socket(zmq.PUB)  # create a publisher socket
+p = "tcp://0.0.0.0:"+ PORT # how and where to communicate
+s.bind(p)  # bind socket to the address
+startTime = datetime.now()
+staticMsg = "Hi! I'm static"
+
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+    time.sleep(5)  # wait every 5 seconds
+    msgTime = str.encode("TIME " + time.asctime())
+    timeDif = datetime.now() - startTime
+    msgLiveTime = str.encode("LIVETIME " + str(timeDif.seconds)+" seconds")
+    msgStaticEncoded = str.encode("STATIC " + staticMsg)
+
+    s.send(msgTime)  # publish the current time
+    s.send(msgLiveTime)  # publish server live time
+    s.send(msgStaticEncoded)  # publish static msg
--- subscriber_2.py
+++ subscriber_2.py
@@ -5,11 +5,10 @@ context = zmq.Context()
 s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
-s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
 s.setsockopt_string(zmq.SUBSCRIBE, "LIVETIME")  # subscribe to server LIVETIME messages
-s.setsockopt_string(zmq.SUBSCRIBE, "STATIC")  # subscribe to STATIC messages
 
-for i in range(6):  # Five iterations
+
+for i in range(100):  # Five iterations
 	time = s.recv()   # receive a message
 	print (bytes.decode(time))
                            
